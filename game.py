b7f1202f4638977053f552ba2d589c45a0765b47
Omair <omair@khan.com>
2018-01-16 23:17:35 +0530

    Commit #2 Such natural movement much wow. 	try to move the charcter along some trap music.

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 import time
 import random
+import sys
 
 
 pygame.init()
@@ -18,25 +19,41 @@ clock = pygame.time.Clock()
 gameDisplay.fill(white)
 pygame.display.update()
 
+charImg = pygame.image.load('character_left.png') 	###Use the correct image
 
-class things:
-	def __init__(self, thingx, thingy, thingw, thingh, color):
-		self.color = color
-		self.thingx = thingx
-		self.thingy = thingy
-		self.thingw = thingw
-		self.thingh = thingh 
+
+def character(x,y):
+    gameDisplay.blit(charImg,(x,y))
+
+
+
+# class things:
+
+
+
+
+# 	def __init__(self, thingx, thingy, thingw, thingh, color):
+# 		self.color = color
+# 		self.thingx = thingx
+# 		self.thingy = thingy
+# 		self.thingw = thingw
+# 		self.thingh = thingh 
 		
 
-	def draw(self):
-		pygame.draw.rect(gameDisplay,self.color,[self.thingx, self.thingy, self.thingw, self.thingh])
-		pygame.display.update()
+# 	def draw(self):
+# 		pygame.draw.rect(gameDisplay,self.color,[self.thingx, self.thingy, self.thingw, self.thingh])
+# 		pygame.display.update()
 
-moveB = things(450,350,20,20,red)
+# moveB = things(450,350,20,20,red)
 # moveB.draw()
 # pygame.display.update()
 	
 def gameLoop():
+	x = (display_width * 0.45)
+	y = (display_height * 0.8)
+
+	x_change = 0			#would have to make it equivalent to velocity
+	y_change = 0			#will introduce gravity in the jump for a realistic movement
 
 	gameExit = False
 
@@ -50,18 +67,29 @@ def gameLoop():
 
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_UP	:
-					moveB.thingy = moveB.thingy - 10
+					y_change = -10
+					# moveB.thingy = moveB.thingy - 150
+				if event.key == pygame.K_DOWN:
+					y_change = 10
+					# moveB.thingy = moveB.thingy + 150
 
 			if event.type == pygame.KEYUP:
 				if event.key == pygame.K_UP:
-					moveB.thingy = moveB.thingy + 10
-		
-			moveB.draw()
-			pygame.display.update()
+					y_change = 10
+
+					# moveB.thingy = moveB.thingy + 150
+				if event.key == pygame.K_DOWN:
+					y_change = -10
+					# moveB.thingy = moveB.thingy - 150 
+
+
+		y += y_change
+		character(x,y)
+		# moveB.draw()
+		pygame.display.update()
 		clock.tick(60)
 
 	# moveB = things(450,350,20,20,red)
-	# moveB.draw()
 
 # pygame.display.update()
 
