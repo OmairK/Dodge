8f01aa56921129a45eaf42a6040d3dad06257abf
Omair <omair@khan.com>
2018-01-18 03:26:58 +0530

    Commit 3 Introduced motion in all direction Fixed the continuous falling or ascending bug

--- game.py
+++ game.py
@@ -6,51 +6,39 @@ import sys
 
 pygame.init()
 
-display_width = 900
-display_height = 700 
+WIDTH = 900
+HEIGHT = 700 
+FPS = 60
 
+
+
+## COLOURS
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
 
-gameDisplay = pygame.display.set_mode((display_width,display_height))
+# Initialize pygame and create a window
+pygame.init()
+gameDisplay = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.mixer.init()
 pygame.display.set_caption('Omair Name')
 clock = pygame.time.Clock()
-gameDisplay.fill(white)
+
 pygame.display.update()
 
-charImg = pygame.image.load('character_left.png') 	###Use the correct image
-
+charImg = pygame.image.load('character_left.png') 	
 
 def character(x,y):
     gameDisplay.blit(charImg,(x,y))
 
 
 
-# class things:
 
 
-
-
-# 	def __init__(self, thingx, thingy, thingw, thingh, color):
-# 		self.color = color
-# 		self.thingx = thingx
-# 		self.thingy = thingy
-# 		self.thingw = thingw
-# 		self.thingh = thingh 
-		
-
-# 	def draw(self):
-# 		pygame.draw.rect(gameDisplay,self.color,[self.thingx, self.thingy, self.thingw, self.thingh])
-# 		pygame.display.update()
-
-# moveB = things(450,350,20,20,red)
-# moveB.draw()
-# pygame.display.update()
 	
 def gameLoop():
-	x = (display_width * 0.45)
-	y = (display_height * 0.8)
+	x = (WIDTH * 0.45)
+	y = (HEIGHT * 0.8)
 
 	x_change = 0			#would have to make it equivalent to velocity
 	y_change = 0			#will introduce gravity in the jump for a realistic movement
@@ -66,32 +54,32 @@ def gameLoop():
 
 
 			if event.type == pygame.KEYDOWN:
+				
 				if event.key == pygame.K_UP	:
-					y_change = -10
-					# moveB.thingy = moveB.thingy - 150
+					y_change = -10	
 				if event.key == pygame.K_DOWN:
 					y_change = 10
-					# moveB.thingy = moveB.thingy + 150
+
+				if event.key == pygame.K_LEFT:
+					x_change = -10
+				if event.key == pygame.K_RIGHT:
+				    x_change = 10				
+					
 
 			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_UP:
-					y_change = 10
-
-					# moveB.thingy = moveB.thingy + 150
-				if event.key == pygame.K_DOWN:
-					y_change = -10
-					# moveB.thingy = moveB.thingy - 150 
-
-
+				if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+					y_change = 0			
+				if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+				    x_change = 0	
 		y += y_change
+		x += x_change
+		gameDisplay.fill(white)
 		character(x,y)
-		# moveB.draw()
 		pygame.display.update()
-		clock.tick(60)
+		clock.tick(FPS)
 
-	# moveB = things(450,350,20,20,red)
 
-# pygame.display.update()
+
 
 gameLoop()
 pygame.quit()
